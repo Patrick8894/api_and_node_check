6ecf7d49293d163840e6985e729522e2a67aa7b4
patrick <patrickwu8894@gmail.com>
2023-12-26 11:23:51 +0800

    test

--- analyze_report.py
+++ analyze_report.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+
 # tree = ET.parse('./error_report.xml')
 tree = ET.parse('./report.xml')
 root = tree.getroot()
